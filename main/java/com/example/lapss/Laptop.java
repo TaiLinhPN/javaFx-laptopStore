988e1e62eed86721633fa3a0ae663a8bb8677cf0
TaiLinhPN <88220193+TaiLinhPN@users.noreply.github.com>
2022-06-13 21:57:49 +0700

    create class laptop

---
+++ main/java/com/example/lapss/Laptop.java
@@ -0,0 +1,20 @@
+package com.example.lapss;
+
+public class Laptop {
+    int id;
+    float price;
+    String name;
+    String img;
+    String company;
+    
+    Laptop(int id, float price,String name,String img, String company) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+        this.img = img;
+        this.company = company;
+
+    }
+    
+
+}
--- main/java/com/example/lapss/Main.java
+++ main/java/com/example/lapss/Main.java
@@ -7,17 +7,52 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        new DBConn();
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+//    public void start(Stage stage) throws IOException {
+//        new DBConn();
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//
+//    }
+    public void start(Stage primaryStage) throws Exception {
+        FlowPane root = new FlowPane();
 
+        root.setPadding(new Insets(15,15,15,15));
+
+        // Button 1
+        Button button1= new Button("Button1");
+        root.getChildren().add(button1);
+
+
+        // Button 2
+        Button button2 = new Button("Button2");
+        button2.setPrefSize(100, 100);
+        root.getChildren().add(button2);
+
+
+
+
+        Scene scene = new Scene(root, 550, 250);
+
+        primaryStage.setTitle("FlowPane Layout Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
